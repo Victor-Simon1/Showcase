61bb99fc29d9b632d2360ec3c931ceaf558df31c
Victor-Simon1 <115700243+Victor-Simon1@users.noreply.github.com>
2025-09-16 15:45:01 +0200

    :rocket: Add outlinable Effect

---
+++ Assets/_Scripts/Effect/OutlinerEffect.cs
@@ -0,0 +1,36 @@
+using EPOOutline;
+using MyUnityPackage.Interactions;
+using MyUnityPackage.Toolkit;
+using UnityEngine;
+
+public class OutlinerEffect : AEffect
+{
+    [SerializeField] Outlinable outlinable;
+
+    public override void OnDisable()
+    {
+        outlinable.enabled = false;
+    }
+
+    public override void OnEnable()
+    {
+
+    }
+
+    public override void OnEnter()
+    {
+        MUPLogger.LogMessageEditor("Je rentre dans la zone");
+        outlinable.enabled = true;
+    }
+
+    public override void OnExit()
+    {
+        MUPLogger.LogMessageEditor("Je sors de la zone");
+        outlinable.enabled = false;
+    }
+
+    public override void OnInteract()
+    {
+
+    }
+}
